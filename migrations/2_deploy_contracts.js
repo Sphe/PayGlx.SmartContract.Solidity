03a60926ba6914e347537ad1c3e4ad8fb87e76c2
sphe <alexandre.lazar@gmail.com>
2018-10-20 00:31:23 +0200

    add deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,11 +12,17 @@ const GlobCoinOwnableToken = artifacts.require('GlobCoinOwnableToken')
 
 module.exports = function(deployer) {
 
-  var publicStorage = deployer.deploy(PublicStorage);
-  var basicTokenLib = deployer.deploy(BasicTokenLib);
-  var standardTokenLib = deployer.deploy(StandardTokenLib);
-  var whitelistingTokenLib = deployer.deploy(WhitelistingTokenLib);
-  var ownableLib = deployer.deploy(OwnableLib);
+  //var publicStorage = deployer.deploy(PublicStorage);
+  //var basicTokenLib = deployer.deploy(BasicTokenLib);
+  //var standardTokenLib = deployer.deploy(StandardTokenLib);
+  //var whitelistingTokenLib = deployer.deploy(WhitelistingTokenLib);
+  //var ownableLib = deployer.deploy(OwnableLib);
+
+  var publicStorage = PublicStorage.at("0x6c06afbc89fe206db9323005606c75b86c05768c");
+  var basicTokenLib = BasicTokenLib.at("0xc700b754cb2f774fae5cb576fdc3f87cabb92e9d");
+  var standardTokenLib = StandardTokenLib.at("0x26e19b2792457c6c7ecaf8cb32a43a7dba7a73ae");
+  var whitelistingTokenLib = WhitelistingTokenLib.at("0x7e5f4500bd286f13793b1cbab283c8729d86297f");
+  var ownableLib = OwnableLib.at("0x6ca8eb0843c132e3381c2250bb6fd05876a36169");
 
   PausableTokenDelegate.link('BasicTokenLib', basicTokenLib.address);
   PausableTokenDelegate.link('StandardTokenLib', standardTokenLib.address);
@@ -37,9 +43,6 @@ module.exports = function(deployer) {
   GlobCoinOwnableToken.link('WhitelistingTokenLib', whitelistingTokenLib.address);
 
   deployer.deploy(GlobCoinOwnableToken);
-
-
-  
 
 };
 
