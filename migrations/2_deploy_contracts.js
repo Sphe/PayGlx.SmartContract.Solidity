9ef30a9090561f85945845970624a7391f10a1de
alexandre.lazar <alexandre.lazar@gmail.com>
2018-10-20 00:11:56 +0200

    fixing truffle settings #3

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,20 +3,43 @@ const BasicTokenLib = artifacts.require('BasicTokenLib')
 const StandardTokenLib = artifacts.require('StandardTokenLib')
 const WhitelistingTokenLib = artifacts.require('WhitelistingTokenLib')
 const OwnableLib = artifacts.require('OwnableLib')
+
+
+
 const PausableTokenDelegate = artifacts.require('PausableTokenDelegate')
 const GlobCoinToken = artifacts.require('GlobCoinToken')
 const GlobCoinOwnableToken = artifacts.require('GlobCoinOwnableToken')
 
 module.exports = function(deployer) {
 
-  deployer.deploy(PublicStorage);
-  deployer.deploy(BasicTokenLib);
-  deployer.deploy(StandardTokenLib);
-  deployer.deploy(WhitelistingTokenLib);
-  deployer.deploy(OwnableLib);
+  var publicStorage = deployer.deploy(PublicStorage);
+  var basicTokenLib = deployer.deploy(BasicTokenLib);
+  var standardTokenLib = deployer.deploy(StandardTokenLib);
+  var whitelistingTokenLib = deployer.deploy(WhitelistingTokenLib);
+  var ownableLib = deployer.deploy(OwnableLib);
+
+  PausableTokenDelegate.link('BasicTokenLib', basicTokenLib.address);
+  PausableTokenDelegate.link('StandardTokenLib', standardTokenLib.address);
+  PausableTokenDelegate.link('WhitelistingTokenLib', whitelistingTokenLib.address);
+
   deployer.deploy(PausableTokenDelegate);
+
+  GlobCoinToken.link('OwnableLib', ownableLib.address);
+  GlobCoinToken.link('BasicTokenLib', basicTokenLib.address);
+  GlobCoinToken.link('StandardTokenLib', standardTokenLib.address);
+  GlobCoinToken.link('WhitelistingTokenLib', whitelistingTokenLib.address);
+
   deployer.deploy(GlobCoinToken);
+
+  GlobCoinOwnableToken.link('OwnableLib', ownableLib.address);
+  GlobCoinOwnableToken.link('BasicTokenLib', basicTokenLib.address);
+  GlobCoinOwnableToken.link('StandardTokenLib', standardTokenLib.address);
+  GlobCoinOwnableToken.link('WhitelistingTokenLib', whitelistingTokenLib.address);
+
   deployer.deploy(GlobCoinOwnableToken);
+
+
+  
 
 };
 
