f16be7b4bf10f92294a0ebe49ae91b0c91fb9ea4
alexandre.lazar <alexandre.lazar@gmail.com>
2018-10-19 21:32:39 +0200

    Adding deploying contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,22 @@
-/* global artifacts */
+const PublicStorage = artifacts.require('PublicStorage')
+const BasicTokenLib = artifacts.require('BasicTokenLib')
+const StandardTokenLib = artifacts.require('StandardTokenLib')
+const WhitelistingTokenLib = artifacts.require('WhitelistingTokenLib')
+const OwnableLib = artifacts.require('OwnableLib')
+const PausableTokenDelegate = artifacts.require('PausableTokenDelegate')
+const GlobCoinToken = artifacts.require('GlobCoinToken')
+const GlobCoinOwnableToken = artifacts.require('GlobCoinOwnableToken')
 
-module.exports = function (deployer) {
-  // deployer.deploy()
-}
+module.exports = function(deployer) {
+
+  deployer.deploy(PublicStorage);
+  deployer.deploy(BasicTokenLib);
+  deployer.deploy(StandardTokenLib);
+  deployer.deploy(WhitelistingTokenLib);
+  deployer.deploy(OwnableLib);
+  deployer.deploy(PausableTokenDelegate);
+  deployer.deploy(GlobCoinToken);
+  deployer.deploy(GlobCoinOwnableToken);
+
+};
+
