70c405b305e0ad7d186a5b1a829f94a7d479b189
alexandre.lazar <alexandre.lazar@gmail.com>
2018-10-20 10:21:03 +0200

    fixing build

--- contracts/Libraries/WhitelistingTokenLib.sol
+++ contracts/Libraries/WhitelistingTokenLib.sol
@@ -6,7 +6,7 @@ import "../Core/Storage/StorageLib.sol";
 library WhitelistingTokenLib {
 
     function getIfWhitelisted(StorageLib.Storage storage self, address who) public view returns (bool) {
-        return self.store.getBool(self.store.bytesToBytes32(abi.encodePacked("whitelisted", who), 0);
+        return self.store.getBool(self.store.bytesToBytes32(abi.encodePacked("whitelisted", who), 0));
     }
 
     function setWhitelisted(StorageLib.Storage storage self, address who, bool isWhitelisted) public {
